f41de1aeca2b7b8c7e642c6ecd1d0edc7821f459
JamesSinnott1994 <JamesRichardSinnott@gmail.com>
2021-02-09 11:47:57 +0000

    [ADD] JS Code added to change Game Page screens

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,43 @@
 $(document).ready(function() {
-    console.log("Hello world!");
+
+    console.log("UP HIGH");
+
+    // Variable declarations
+    let topic = "";
+    let difficulty = "";
+
+    // Binds a click event to each button in the topic section
+    $("#topic-section button").each(function() {
+        $(this).bind('click', function() {
+
+            // Assign topic to button that was clicked on
+            topic = $(this).html();
+            alert(`User clicked on ${topic}`);
+
+            // Hide Topic screen
+            $("#topic-screen").hide();
+             $("#topic-header").hide();
+
+            // Display difficulty screen
+            $("#difficulty-screen").show();
+             $("#difficulty-header").show();
+        });
+    });
+
+    // Binds a click event to each button in the difficulty section
+    $("#difficulty-section button").each(function() {
+        $(this).bind('click', function() {
+
+            // Assign topic to button that was clicked on
+            difficulty = $(this).html();
+            alert(`User clicked on ${difficulty}`);
+
+            // Hide Difficulty screen
+            $("#difficulty-screen").hide();
+             $("#difficulty-header").hide();
+        });
+    });
+
+    console.log("DOWN LOW");
+
 });
