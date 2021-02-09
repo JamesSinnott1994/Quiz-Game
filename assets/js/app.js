29bb9a038e35a11dba4f796ceadf20a240de08c4
JamesSinnott1994 <JamesRichardSinnott@gmail.com>
2021-02-09 14:44:08 +0000

    [ADD] Code added to Retrieve API Quiz data

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,14 +6,14 @@ $(document).ready(function() {
     let topic = "";
     let difficulty = "";
 
-    // Binds a click event to each button in the topic section
+    // TOPIC SCREEN
+    // Binds a click event to each button on the Topic screen
     $("#topic-section button").each(function() {
         $(this).bind('click', function() {
 
             // Assign topic to button that was clicked on
-            topic = $(this).html();
+            topic = $(this).val();
             alert(`User clicked on ${topic}`);
-
             // Hide Topic screen
             $("#topic-screen").hide();
              $("#topic-header").hide();
@@ -24,19 +24,47 @@ $(document).ready(function() {
         });
     });
 
-    // Binds a click event to each button in the difficulty section
+    // DIFFICULTY SCREEN
+    // Binds a click event to each button on the Difficulty screen
     $("#difficulty-section button").each(function() {
         $(this).bind('click', function() {
 
             // Assign topic to button that was clicked on
-            difficulty = $(this).html();
+            difficulty = $(this).html().toLowerCase();
             alert(`User clicked on ${difficulty}`);
 
             // Hide Difficulty screen
             $("#difficulty-screen").hide();
              $("#difficulty-header").hide();
+
+             // Call function to retrieve API Data
+             getAPIData(printDataToConsole);
         });
     });
+
+    // RETRIEVE QUESTIONS FROM API
+    function getAPIData(callback) { // "callback" = "printDataToConsole"
+        var xhr = new XMLHttpRequest();
+
+        if (difficulty != "random") {
+            xhr.open("GET", `https://opentdb.com/api.php?amount=100&category=${topic}&difficulty=${difficulty}`);
+        } else {
+            xhr.open("GET", `https://opentdb.com/api.php?amount=100&category=${topic}`);
+        }
+        xhr.send();
+
+        xhr.onreadystatechange = function() {
+            if (this.readyState == 4 && this.status == 200) {
+                callback(JSON.parse(this.responseText)); // Line 9
+            }
+        };
+    }
+
+    function printDataToConsole(data) {
+        console.log(data);
+    }
+    
+    // GAME SCREEN
 
     console.log("DOWN LOW");
 
