6a2096af87c9cf3a468d5c43bba3fd434a0ae670
JamesSinnott1994 <JamesRichardSinnott@gmail.com>
2021-02-27 14:11:44 +0000

    [ADD] Smooth focus on continue button

--- assets/js/app.js
+++ assets/js/app.js
@@ -234,8 +234,6 @@ $(document).ready(function() {
                 timerStopped = true;
 
                 if (answer == correctAnswer) {
-                    //alert("CORRECT ANSWER!");
-
                     // https://medium.com/@ericschwartz7/adding-audio-to-your-app-with-jquery-fa96b99dfa97
                     $("#correct-sound")[0].play();
 
@@ -250,6 +248,8 @@ $(document).ready(function() {
                     noOfCorrectAnswers += 1;
                     $("#score").html(score);
                     $("#questions-answered").html(questionsAnswered);
+
+                    smoothFocus();
                 } else {
                     $("#wrong-sound")[0].play();
                     // Display animation
@@ -260,6 +260,8 @@ $(document).ready(function() {
 
                     enableContinueBtn();
                     disableAnswerBtns();
+
+                    smoothFocus();
                 }
             });
         });
@@ -351,6 +353,36 @@ $(document).ready(function() {
     }
 
 });
+
+function smoothFocus() {
+    /*
+    For help with smoothly focusing on the continue button, I got help from the following source:
+    https://css-tricks.com/snippets/jquery/smooth-scrolling/
+    */
+
+    var target = $("#continue-btn");
+    target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+    // Does a scroll target exist?
+    if (target.length) {
+    // Only prevent default if animation is actually gonna happen
+    event.preventDefault();
+    $('html, body').animate({
+        scrollTop: target.offset().top
+    }, 2000, function() {
+        // Callback after animation
+        // Must change focus!
+        var $target = $(target);
+        $target.focus();
+        if ($target.is(":focus")) { // Checking if the target was focused
+        return false;
+        } else {
+        $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+        $target.focus(); // Set focus again
+        };
+    });
+    }
+
+}
 
 function enableContinueBtn() {
     $("#continue-btn").css('opacity', '1');
